12f4d273f32bedc8d42392e1e9fe6e336eb25b07
Jonttufantti <81356143+Jonttufantti@users.noreply.github.com>
2024-11-14 16:24:20 +0200

    sacdd

--- viikko45/perjantai/backend/myproject/base/models.py
+++ viikko45/perjantai/backend/myproject/base/models.py
@@ -1,47 +1,5 @@
 from django.db import models
 
-class Kayttaja(models.Model):
-    tunnus = models.AutoField(primary_key=True)
-    nimi = models.CharField(max_length=100)
-    salasana = models.CharField(max_length=100)
-
-    class Meta:
-        db_table = 'kayttajat'
-
-    def __str__(self):
-        return self.nimi
-
-
-class Tilat(models.Model):
-    id = models.AutoField(primary_key=True)
-    tilan_nimi = models.CharField(max_length=16)
-
-    class Meta:
-        db_table = 'tilat'
-
-    def __str__(self):
-        return self.tilan_nimi
-
-
-class Varaajat(models.Model):
-    id = models.AutoField(primary_key=True)
-    nimi = models.CharField(max_length=64)
-
-    class Meta:
-        db_table = 'varaajat'
-
-    def __str__(self):
-        return self.nimi
-
-
-class Varaukset(models.Model):
-    id = models.AutoField(primary_key=True)
-    tila = models.ForeignKey(Tilat, on_delete=models.CASCADE)
-    varaaja = models.ForeignKey(Varaajat, on_delete=models.CASCADE)
-    varauspaiva = models.DateField()
-
-    class Meta:
-        db_table = 'varaukset'
-
-    def __str__(self):
-        return f"{self.varauspaiva} - {self.tila} by {self.varaaja}"
+class Item(models.Model):
+    name = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
--- viikko45/perjantai/backend/tilavaraus/tilavaraus_api/settings.py
+++ viikko45/perjantai/backend/tilavaraus/tilavaraus_api/settings.py
@@ -97,7 +97,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'tilavaraus',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': 'root',
         'HOST': 'localhost',
         'PORT': '3306',
     }
